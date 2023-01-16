c07caf9709ef35da6843d216974c8a4113034fe8
Killian Rak <kikirakoto1602@gmail.com>
2023-01-16 18:35:43 +0100

    spawn d'objet et disparition au hasard fonctionnel

--- Assets/Scripts/TreasureSpawn.cs
+++ Assets/Scripts/TreasureSpawn.cs
@@ -9,6 +9,16 @@ public class TreasureSpawn : MonoBehaviour
     [SerializeField]
     private Transform[] spawners;
     private GameObject treasure;
+    private bool left = false;
+    private bool right = false;
+    private bool center= false;
+    private List<bool> position = new List<bool>();
+
+    void Awake(){
+        position.Add(left);
+        position.Add(right);
+        position.Add(center);
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +34,22 @@ public class TreasureSpawn : MonoBehaviour
     private void Spawn(){
         int indexTreasure = Random.Range(0, 3);
         int indexSpawner = Random.Range(0, 3);
+    
+    while(position[indexSpawner] == false){
         treasure = Instantiate(spawnObjects[indexTreasure]);
         treasure.transform.position = spawners[indexSpawner].transform.position;
+        position[indexSpawner] = true;
+        StartCoroutine(DestroyAfter(treasure, position, indexSpawner));        
+    } 
+
+
+    }
+
+    private IEnumerator DestroyAfter(GameObject obj, List<bool> pos, int index)
+    {
+            yield return new WaitForSeconds(Random.Range(0, 20));
+            Destroy(obj);
+            pos[index] = false;
 
     }
 }
